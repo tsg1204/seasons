44a6a16affc7f322a80ed5a7c82518368fa8ec17
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-27 13:27:07 -0500

    add cond rendering test

--- src/index.js
+++ src/index.js
@@ -3,17 +3,32 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message })
+      }
+    )
+
+  }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
-    )
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
-    return (
-      <div>Latitude: </div>
-    )
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
