1d4841a9c637630dfb1c6ad6c681c977ba46670d
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-27 14:00:25 -0500

    season disp comp changed

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+      return lat > 0 ? 'summer' : 'winter';
+    } else {
+      return lat > 0 ? 'winter' : 'summer';
+    }
+  };
+  
+  const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text =
+      season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  
     return (
-        <div>Season Display</div>
-    )
-}
-
+      <div>
+        <h1>{text}</h1>
+      </div>
+    );
+  };
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 
 class App extends React.Component {
   // constructor(props) {
@@ -24,7 +26,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>;
