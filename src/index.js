1a37b28a02e4c1942381cb4f5758f5211f821454
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-27 15:02:07 -0500

    added spinner comp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
@@ -29,7 +30,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
